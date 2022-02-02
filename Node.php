82e838566be5eedf27899a38657a83fd4c622ada
VallraFTM <sagdacov@mail.ru>
2022-02-02 16:08:46 +0300

    quick sort

---
+++ Node.php
@@ -0,0 +1,16 @@
+<?php
+class Node {
+    public $name = '';
+    public $id = NULL;
+    public $parent_id = NULL;
+
+    public function __construct($name) {
+        $this->name = $name;
+    }
+
+    public function addChild (Node) {
+        
+    }
+
+
+}
